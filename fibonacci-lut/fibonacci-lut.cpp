c2ca3ba46ec8245c18e74512f38a32bc46c9e11d
primeminister89 <prime@leech.it>
2012-05-06 02:12:15 +0200

    Checkarray is falsed when initialized. Some Comments added.

--- fibonacci-lut/fibonacci-lut.cpp
+++ fibonacci-lut/fibonacci-lut.cpp
@@ -3,6 +3,8 @@
  *
  *  Created on: 05.05.2012
  *      Author: prime
+ *
+ *      Fragen in den Kommentaren
  */
 
 
@@ -13,11 +15,9 @@ class FibonacciLookupTable {
 public:
 	FibonacciLookupTable(unsigned int number_of_elements);
 	~FibonacciLookupTable();
-	unsigned int get_fibo_at(int n); //prints specific fibo, the nth fibo
-
-	//mehr funktionalität
+	unsigned int get_fibo_at(int n); // returns the fibo with index n
 	void calculate_fibo(int index); //kein unsigned int, compiler meckert dann wegen pow()
-	void get_fibo_series(); //prints the whole fibo series !!get->print ersetzen oder array zurückgeben
+									   //was tun auser auf unsigned int verzichten?
 
 
 private:
@@ -28,9 +28,10 @@ private:
 
 FibonacciLookupTable::FibonacciLookupTable(unsigned int number_of_elements) {
 	first_fibo_ptr = new unsigned int[number_of_elements];
-	checker_ptr = new bool[number_of_elements]; //Anderer Ansatz, zweidim array?
+	checker_ptr = new bool[number_of_elements]; //Besserer Ansatz, zweidim array?
 	//Wie setze ich hier elegant das Ganze array auf false?
-	for (int i=0; i<number_of_elements; ++i) {
+	//bool[number_of_elements] = {false} will er hier nicht
+	for (unsigned int i=0; i<number_of_elements; ++i) {
 		*(checker_ptr + i) = false;
 	}
 }
@@ -42,10 +43,6 @@ FibonacciLookupTable::~FibonacciLookupTable() {
 	checker_ptr = 0;
 }
 
-void FibonacciLookupTable::get_fibo_series() {
-
-}
-
 unsigned int FibonacciLookupTable::get_fibo_at(int n) {
 	calculate_fibo(n);
 	return  (*(first_fibo_ptr + n));
@@ -53,13 +50,11 @@ unsigned int FibonacciLookupTable::get_fibo_at(int n) {
 
 void FibonacciLookupTable::calculate_fibo(int index) {
 	if (*(checker_ptr + index) == false) {
-
-	//Uses Moivre-Binet
-	*(first_fibo_ptr + index) = std::floor(1.0/std::sqrt(5) *
+		//Uses Moivre-Binet
+		*(first_fibo_ptr + index) = std::floor(1.0/std::sqrt(5) *
 			(std::pow((0.5*(1+std::sqrt(5))),index) - std::pow((0.5*(1-std::sqrt(5))),index)));
-
-	*(checker_ptr + index) = true;
-	std::cout << "\nWert berechnet und abgespeichert.\n";
+		*(checker_ptr + index) = true;
+		std::cout << "\nWert berechnet und abgespeichert.\n";
 	} else {
 		std::cout << "\nWert bereits berechnet.\n";
 	}
@@ -69,10 +64,10 @@ void FibonacciLookupTable::calculate_fibo(int index) {
 
 int main() {
 	FibonacciLookupTable LUT(10);
-	unsigned int an_element;
+	unsigned int x;
 	std::cout << "Das wievielte Element?: ";
-	std::cin >> an_element;
-	std::cout << LUT.get_fibo_at(an_element);
-	std::cout << LUT.get_fibo_at(an_element);
+	std::cin >> x;
+	std::cout << LUT.get_fibo_at(x);
+	std::cout << LUT.get_fibo_at(x);
 
 }
