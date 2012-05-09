3fbd7af9198f0add203da9c83ba6b150778fc68c
primeminister89 <prime@leech.it>
2012-05-09 21:59:52 +0200

    I have no idea what I am doing.;)

--- ringpuffer/ringpuffer.cpp
+++ ringpuffer/ringpuffer.cpp
@@ -16,8 +16,8 @@ public:
 	void print_puffer(); //printed alle daten im Puffer, beginnend mit ältesten element (fifo)
 	//TODO: Array als Rückgabewert
 private:
-	double *first_memory; //points to the first empty element in array
-	bool *first_check;
+	double *first_memory_ptr; //points to the first empty element in array
+	bool *first_check_ptr;
 	unsigned int input;
 	unsigned int output;
 	unsigned int size;
@@ -28,27 +28,27 @@ Ringpuffer::Ringpuffer(unsigned int number_of_elements) {
 	output = 0; //location of last output
 	size = number_of_elements;
 
-	first_memory = new double[number_of_elements];
-	first_check = new bool[number_of_elements];
+	first_memory_ptr = new double[number_of_elements];
+	first_check_ptr = new bool[number_of_elements];
 	for (unsigned int i=0; i<size; ++i) {
-		first_memory[i] = 0;
-		first_check[i] = true;
+		first_memory_ptr[i] = 0;
+		first_check_ptr[i] = true;
 	}
 }
 
 //Gibt es hier wieder was besseres wie 2 arrays?
 Ringpuffer::~Ringpuffer() {
 	//clean heap
-	delete[] first_memory;
-	first_memory = 0;
-	delete[] first_check;
-	first_check = 0;
+	delete[] first_memory_ptr;
+	first_memory_ptr = 0;
+	delete[] first_check_ptr;
+	first_check_ptr = 0;
 }
 
 bool Ringpuffer::push(double date) {
-	if (first_check[input] == true) {
-		first_memory[input] = date;
-		first_check[input] = false;
+	if (first_check_ptr[input] == true) {
+		first_memory_ptr[input] = date;
+		first_check_ptr[input] = false;
 		++input;
 		if (input == size) {
 			input = 0;
@@ -62,43 +62,58 @@ bool Ringpuffer::push(double date) {
 }
 
 double Ringpuffer::pop() {
-	first_check[output] = true;
-	double temp = first_memory[output];
+	first_check_ptr[output] = true;
+	double temp = first_memory_ptr[output];
 	++output;
 	if (output == size) {
 		output = 0;
 	}
+	std::cout << temp << std::endl;
 	return temp;
 }
 
+//haha, muss selber bisschen lachen darüber. Hab aber kein Kopf mir grad mehr Gedanken zu machen
 void Ringpuffer::print_puffer() {
-	for (unsigned int i=0; i<size; ++i) {
-		std::cout << first_memory[output+i] << std::endl;
+	unsigned int elements_printed = 0;
+	for (unsigned int i=0; output+i<size; ++i) {
+		std::cout << std::endl << first_memory_ptr[output+i];
+		++elements_printed;
 	}
+	for (unsigned int i=0; i<size-1-output; ++i) {
+		if (elements_printed == size)
+			break;
+		std::cout << std::endl << first_memory_ptr[i];
+		++elements_printed;
+	}
+	std::cout << std::endl;
 }
 
 //Nur Testcode ab hier...
 int main() {
 	Ringpuffer puffer(3);
-	//double temp;
-
+	std::cout << "Zahlen von 0 bis 4 Versuchen einzuspeichern. Kein Platz fuer die 4 im Puffer" <<std::endl;
 	for (unsigned int i=0; i<4; ++i) {
-		//std::cout << "##double speichern: ";
-		//std::cin >> temp;
 		puffer.push(i);
 	}
+	std::cout << "----------------------------------------" << std::endl;
 
+	std::cout << "Pufferinhalt ausgeben" << std::endl;
 	puffer.print_puffer();
+	std::cout << "----------------------------------------" << std::endl;
 
+	std::cout << "Erstes eingebenes Element anzeigen" << std::endl;
 	puffer.pop();
+	std::cout << "----------------------------------------" << std::endl;
 
+	std::cout << "Zahlen von 4 bis 6 Versuchen einzuspeichern. Kein Platz für 5,6 im Puffer" << std::endl;
 	for (unsigned int i=4; i<7; ++i) {
-		//std::cout << "##double speichern: ";
-		//std::cin >> temp;
 		puffer.push(i);
 	}
+	std::cout << "----------------------------------------" << std::endl;
 
+	std::cout << "Pufferinhalt ausgeben" << std::endl;
 	puffer.print_puffer();
+	std::cout << "----------------------------------------" << std::endl;
 
 }
 
